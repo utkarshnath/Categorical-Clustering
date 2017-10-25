43a0ac3a279e2b27acd8366324b20f98372d426b
utkarshnath <nath.utkarsh1@gmail.com>
2017-10-25 10:27:54 +0530

    added brest cancer data

--- major.py
+++ major.py
@@ -7,13 +7,26 @@ from scipy.stats.stats import pearsonr
 from scipy.spatial import distance
 import skfuzzy as fuzz
 
-lines = np.loadtxt("fitting_lenses.txt")
-X = lines[:, [1, 2, 3, 4]]
-Y = lines[:, [5]]
-m = 4
-N = 24
+lines = np.loadtxt("breast_cancer_data.txt",delimiter=',')
+X = lines[:, [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+Y = lines[:, [10]]
+m = 9
+N = 699
+number_of_clusters = 2
+
+# lines = np.loadtxt("fitting_lenses.txt")
+# X = lines[:, [1, 2, 3, 4]]
+# Y = lines[:, [5]]
+# m = 4
+# N = 24
+# number_of_clusters = 3
+
+
 for i in xrange(0,N):
-    Y[i]-=1
+    if(Y[i]==2):
+        Y[i]=0
+    else:
+        Y[i]=1
 def theta(a,b):
     similarity = 0;
     for i in xrange(0,m):
@@ -80,27 +93,27 @@ def count(Y,label,number_of_classes):
 
     return similarLabels/(N*1.0)
 
-kmeans = KMeans(n_clusters=3,n_init=100,tol=0.00001).fit(data)
+kmeans = KMeans(n_clusters=number_of_clusters,n_init=100,tol=0.00001).fit(data)
 print "---------- SBC -------------"
-print kmeans.labels_
-print count(Y,kmeans.labels_,3)
+# print kmeans.labels_
+print count(Y,kmeans.labels_,number_of_clusters)
 print "----------------------------"
 
-km = KModes(n_clusters=3, n_init=100).fit(X)
+km = KModes(n_clusters=number_of_clusters, n_init=100).fit(X)
 print "---------- kmodes ----------"
-print km.labels_
-print count(Y,km.labels_,3)
+# print km.labels_
+print count(Y,km.labels_,number_of_clusters)
 print "----------------------------"
 
-bandwidth = estimate_bandwidth(data, quantile=0.2, n_samples=N)
-ms = MeanShift(bandwidth=bandwidth).fit(data)
-print "---------- Mean Shift ----------"
-print ms.labels_
-print count(Y,ms.labels_,3)
-print "----------------------------"
+# bandwidth = estimate_bandwidth(data, quantile=0.2, n_samples=N)
+# ms = MeanShift(bandwidth=bandwidth).fit(data)
+# print "---------- Mean Shift ----------"
+# print ms.labels_
+# print count(Y,ms.labels_,number_of_clusters)
+# print "----------------------------"
 
-fa = FeatureAgglomeration(n_clusters=3).fit(data)
+fa = FeatureAgglomeration(n_clusters=number_of_clusters).fit(data)
 print "---------- Hierarchical  ----------"
-print fa.labels_
-print count(Y,fa.labels_,3)
+# print fa.labels_
+print count(Y,fa.labels_,number_of_clusters)
 print "----------------------------"
